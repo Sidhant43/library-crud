f9eda11ad29a5f36388eab59c62d1b4c47a0e35f
sidhant <136330213+Sidhant43@users.noreply.github.com>
2023-12-04 17:56:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Import CRUD operations from separate files
+const getAllBooksRouter = require("./app/books/getAllBooks");
+const addNewBookRouter = require("./app/books/addNewBook");
+const updateBookDetailsRouter = require("./app/books/updateBookDetails");
+
+// Use the CRUD operations routers
+app.use(getAllBooksRouter);
+app.use(addNewBookRouter);
+app.use(updateBookDetailsRouter);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ updateBookDetails.js
@@ -0,0 +1,8 @@
+
+const express = require("express");
+const mysql = require("mysql2/promise");
+
+const router = express.Router();
+
+
+module.exports = router;
